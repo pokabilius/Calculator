0a4f2014a9e21ac8e14c9d9fe20e5105209314ba
pokabilius <georgemirsilidis@hotmail.com>
2023-02-19 02:56:43 +0200

    Enter key and C pending

--- main.js
+++ main.js
@@ -1,96 +1,184 @@
 // Variables-------------------------------
 const buttons = document.querySelectorAll(".numbers")
 const operationBtns = document.querySelectorAll(".symbols")
-const screen = document.querySelector(".screen")
+const screenUp = document.querySelector("#up")
+const screenDown = document.querySelector("#down")
 let number = ""
-let symbol = ""
+let lastsymbol = ""
+let previousSymbol = 0
 let storedSymbol = []
 let storedNumber = []
 let firstNumber = 0
 let secondNumber = 0
 let result = 0
-  
+let lastResult = 0
+let helpNumber = 0
+let operationPending = false
+let allArray = []
+
 // Grab number from keys
 
 buttons.forEach((button) => {
-
+    
     button.addEventListener('click', (e) => {
        number = parseInt(e.target.textContent)
        storedNumber.push(number)
        console.log(storedNumber)
+       screenDown.textContent = screenDown.textContent + number
+       allArray.push(number)
        
     })
 })
 
 // Grab symbols from keys
-     
-operationBtns.forEach((button) => {
 
+operationBtns.forEach((button) => {
+    
     button.addEventListener('click', (e) => {
-        symbol = e.target.textContent
-        storedSymbol.push(symbol) 
-       console.log(storedSymbol)
+        lastsymbol = e.target.textContent
+        storedSymbol.push(lastsymbol) 
+        console.log(storedSymbol)
+        allArray.push(lastsymbol)
         pressOperator()
+        console.log(allArray)
     })
     
 })
 
-// Pseudocode-----------------------------
-// Check which operation is pressed and change the array to store second number
-// when second operator or enter is pressed do the math betwwen the first two arrays
-// We need an array to store everything at all times and when two symbols are found break the arrays into two and convert to numbers and do the operation
-// we need a deleter to delete the array when we reach 2nd operator
+
+
+// function checkNumberOfSymbols (array) {
+//     if (array.length % 2 === 0){
+//         console.log ("even")
+//         return operationPending = true
+//     }else {
+//         console.log ("odd")
+//         return operationPending = false
+//     }
+// }
+
+function resetArray (name) {
+    name.length = 0
+}
+
+function deleteScreen () {
+    screenUp.textContent = ""
+    screenDown.textContent = ""
+
+}
+
+// Operation functions
+function add(a,b) {
+    return parseFloat(a) + parseFloat(b) 
+}
+
+function subtract (a,b){
+    return parseFloat(a) - parseFloat(b) 
+}
+
+function multiply (a,b){
+    return parseFloat(a) * parseFloat(b) 
+}
+
+function divide (a,b){
+    return parseFloat(a) / parseFloat(b) 
+}
+
+function power (a,b){
+    return parseFloat(a) ** parseFloat(b)
+}
+
 
 function pressOperator() {
     
-    if (storedNumber.length === 0 && storedSymbol.length === 1) {
-        console.log("empty number return")
-        storedSymbol.length = 0
+    if (lastsymbol === "DELETE"){
+        resetArray(storedNumber)
+        resetArray(storedSymbol)
+        deleteScreen()
+        lastResult = 0
+    }
+    if (isNaN(allArray[allArray.length -1]) && isNaN(allArray[allArray.length -2]) && allArray[allArray.length - 1] != "DELETE"){
+        screenUp.textContent = lastResult + lastsymbol
         return
     }
-    if (storedSymbol.length  === 1) {
-        //take the number in 
-        firstNumber = storedNumber.join("")
-        // show the number in the screen and show the operator
-        screen.textContent =  `${firstNumber} ${storedSymbol[0]}`
-        storedNumber.length = 0
-        console.log(firstNumber)
-        
+    
+    if (storedNumber.length === 0 && !storedNumber.length <= 1) {
+        console.log("empty number return")
+        resetArray(storedSymbol)
+        return
+    } else {
+            secondNumber = storedNumber.join("")
+            resetArray(storedNumber)
+            if (storedSymbol.length <=1){
+                previousSymbol= storedSymbol[storedSymbol.length - 1]
+            } else {
+                previousSymbol = storedSymbol[storedSymbol.length - 2]
+            }
+            console.log(previousSymbol)
 
-    }else if (storedSymbol.length === 2){
-        if (storedNumber.length === 0 ){
-            storedSymbol.pop()
-            return
-        }
-        secondNumber = storedNumber.join("")
-        console.log(secondNumber)
+            switch (previousSymbol) {
+                case "+" :
+                    lastResult = add(secondNumber,lastResult)
+                    screenDown.textContent = ""
+                    screenUp.textContent = lastResult + lastsymbol
+                    break
+                case "-" :
+                    if (storedSymbol.length <= 1) {
+                        screenDown.textContent = ""
+                        screenUp.textContent = secondNumber + lastsymbol
+                        lastResult = secondNumber
+                    }else {
+                        lastResult = subtract(lastResult,secondNumber)
+                        screenDown.textContent = ""
+                        screenUp.textContent = lastResult + lastsymbol
+                        
+                    }
+                    break
+                case "/" :
+                    if (storedSymbol.length <= 1) {
+                        screenDown.textContent = ""
+                        screenUp.textContent = secondNumber + lastsymbol
+                        lastResult = secondNumber
+                    }else {
+                        lastResult = divide(lastResult,secondNumber)
+                        console.log(lastResult)
+                        console.log(secondNumber)
+                        screenDown.textContent = ""
+                        screenUp.textContent = lastResult + lastsymbol
+                    }
+                    break
+                case "*" :
+                    if (storedSymbol.length <= 1) {
+                        // helpNumber = secondNumber
+                        screenDown.textContent = ""
+                        screenUp.textContent = secondNumber + lastsymbol
+                        lastResult = secondNumber
+                    }else {
+                        lastResult = multiply(lastResult,secondNumber)
+                        console.log(lastResult)
+                        console.log(secondNumber)
+                        screenDown.textContent = ""
+                        screenUp.textContent = lastResult + lastsymbol
+                    }
+                    break
+                case "^" :
+                    if (storedSymbol.length <= 1) {
+                        screenDown.textContent = ""
+                        screenUp.textContent = secondNumber + lastsymbol
+                        lastResult = secondNumber
+                    }else {
+                        lastResult = power(lastResult,secondNumber)
+                        console.log(lastResult)
+                        console.log(secondNumber)
+                        screenDown.textContent = ""
+                        screenUp.textContent = lastResult + lastsymbol
+                    }
+                    break
+            }
 
-        // show the resutl 
-        if (storedSymbol[0] = "+"){
-            result = parseFloat(firstNumber)  + parseFloat(secondNumber)
-            screen.textContent = ""
-            screen.textContent = secondNumber
-            screen.textContent = result
-            console.log(`result ${result} = ${firstNumber} + ${secondNumber}`)
-        }
-        storedNumber.length = 0
-        
-    }else if (storedSymbol.length = 3) {
-        if (storedNumber.length === 0 ){
-            storedSymbol.pop()
-            return
-        }
-        secondNumber = storedNumber.join("")
-        if (storedSymbol[1] = "+"){
-            let secondresult = parseFloat(result)  + parseFloat(secondNumber)
-            screen.textContent = ""
-            screen.textContent = secondresult
-            console.log(storedNumber)
-            console.log(`ENTER THIRD CASE THE SECOND OPERATOR IS ${storedSymbol[1]} AND THE RESULT IS ${secondresult} = ${result} + ${secondNumber}`)
-        }
-        storedSymbol.length = 0
-        storedNumber.length = 0
     }
-
 }    
 
+    
+    
+    
